59f3a86303ba9d684587d5d3087797b8423d992c
daubejb <daubejb@gmail.com>
2020-09-30 14:02:39 -0400

    hello github

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from "svelte";
+  import ArtistList from "./ArtistList.svelte";
+  import ArtistSearch from "./ArtistSearch.svelte";
+  let searchTerm = "Jeffrey B. Daube";
+  let artists = [];
+
+  onMount(async () => {
+    const res = await fetch(`data.json`);
+    artists = await res.json();
+  });
+</script>
+
+<style global lang="scss">
+  $primary: green;
+
+  @import "../node_modules/bootstrap/scss/bootstrap.scss";
+</style>
+
+<div class="container">
+  <ArtistSearch {searchTerm} />
+  <ArtistList {artists} />
+</div>
